a4df2fa32a5a0756b9218e4d992ff9ef9da77837
nozipho97 <mayilanozipho@gmail.com>
2023-07-20 16:29:15 +0200

    first commit

---
+++ learn-javascript-main/1. Build a Passenger Counter App/03. Setting up our JavaScript file/index.js
@@ -0,0 +1,9 @@
+// document.getElementById("count-el").innerText = 5
+let count = 0
+console.log(count)
+
+// 1. create a variable, myAge, and set its value to your age
+let myAge = 26
+
+//2. log the myAge variable to the console
+console.log(myAge)
